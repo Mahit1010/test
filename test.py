8cbf39df55f1326d9135e168c908bfd37ffbedc5
Mahit1010 <mahitk06@gmail.com>
2024-09-09 15:07:19 +0530

    Create test.py
    dedabadodabadedayogo hes a shadow

---
+++ test.py
@@ -0,0 +1,1 @@
+print('oh god who art truth and the origin of all knowledge bless our studies which concecrate to u, enlighten our minds strengthen our memories and direct our wills towards what is right.')
